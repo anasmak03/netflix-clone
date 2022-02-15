8c385e5b15922317a56d8fe17bc8d78fe2c5ba4f
anasmak03 <anas11.makhloufi@gmail.com>
2022-02-15 21:26:41 +0100

    netflix clone

---
+++ pages/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function About (){
+    
+        return (
+            <div>
+                
+            </div>
+        );
+    
+}
+
+export default About;
---
+++ pages/Homee.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+import Image from 'next/image';
+function Homee (){
+    
+        return (
+            <div>
+               
+            </div>
+        );
+    
+}
+
+export default Homee;
